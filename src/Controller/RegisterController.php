979604e206c4f18646f5157ac72bbe50fcad9bd3
yohannzaoui <yohannzaoui@gmail.com>
2018-09-30 14:20:11 +0200

    update register function

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -17,23 +17,33 @@ class RegisterController extends AbstractController
     /**
      * @Route("/inscription", name="register")
      */
-    public function index(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder)
+    public function index(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder, \Swift_Mailer $mailer)
     {
 
         $user = new User;
         $user->setCreatedAt(new \DateTime);
+        
         $form = $this->createFormBuilder($user)
             ->add('username', TextType::class)
             ->add('email', EmailType::class)
             ->add('password', PasswordType::class)
-            //->add('confirmePassword', PasswordType::class)
+            ->add('confirmePassword', PasswordType::class)
             ->getForm();
+
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $hash = $encoder->encodePassword($user, $user->getPassword());
             $user->setPassword($hash);
+            
+            $message = (new \Swift_Message('Hello Email'))
+                        ->setFrom('register@snowtrick.com')
+                        ->setTo($request->request->get('email'))
+                        ->setBody('TEST');
+            $mailer->send($message);
+
             $manager->persist($user);
             $manager->flush();
+
             return $this->redirectToRoute('login');
         }
         return $this->render('register/index.html.twig', [
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -47,6 +47,16 @@ class User implements UserInterface
    */
     private $createdAt;
 
+    /**
+     * @ORM\Column(name="token", type="string", length=255)
+     */
+    private $token;
+
+    /**
+     * @ORM\Column(name="ctoken", type="string", length=255)
+     */
+    private $ctoken;
+
     
 
     /**
@@ -164,4 +174,64 @@ class User implements UserInterface
         return ['ROLE_USER'];
     }
 
+
+    /**
+     * Get the value of token
+     */ 
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * Set the value of token
+     *
+     * @return  self
+     */ 
+    public function setToken($token)
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of ctoken
+     */ 
+    public function getCtoken()
+    {
+        return $this->ctoken;
+    }
+
+    /**
+     * Set the value of ctoken
+     *
+     * @return  self
+     */ 
+    public function setCtoken($ctoken)
+    {
+        $this->ctoken = $ctoken;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of confirmePassword
+     */ 
+    public function getConfirmePassword()
+    {
+        return $this->confirmePassword;
+    }
+
+    /**
+     * Set the value of confirmePassword
+     *
+     * @return  self
+     */ 
+    public function setConfirmePassword($confirmePassword)
+    {
+        $this->confirmePassword = $confirmePassword;
+
+        return $this;
+    }
 }
